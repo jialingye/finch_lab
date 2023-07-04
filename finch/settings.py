4f7c51a1edbb930eb63d07b2a823d89a3d7fda23
Jialing Ye <jialingye@Jialings-MacBook-Pro.local>
2023-07-03 20:29:52 -0500

    add host

--- finch/settings.py
+++ finch/settings.py
@@ -34,6 +34,9 @@ DEBUG ='RENDER' not in os.environ
 
 ALLOWED_HOSTS = []
 
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
@@ -104,7 +107,7 @@ WSGI_APPLICATION = 'finch.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         # Feel free to alter this value to suit your needs.
-        default='postgres://jialingye:izWXDuv9dZB8@ep-summer-morning-631731.us-west-2.aws.neon.tech/country',
+        
         conn_max_age=600
     )
 }
